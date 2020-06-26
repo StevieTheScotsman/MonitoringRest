96830e7f0692994bf9566297617a863844a5f7ad
Stephen Curran <steve.curran@cubicdesigns.com>
2020-06-26 11:41:20 -0500

    initial commit

---
+++ Controllers/MonitoringController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Monitoring.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Monitoring.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonitoringController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public MonitoringController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        // GET: api/Monitoring
+        [HttpGet]
+        public IEnumerable<MonitorRecord> Get()
+        {
+            IEnumerable<MonitorRecord> monitorRecords = Monitoring.Persistance.SqlCommandUtility.GetAll(_configuration);
+            monitorRecords = monitorRecords.OrderByDescending(x => x.ModifiedDate).ToList();
+            return monitorRecords;
+        }
+
+        // GET: api/Monitoring/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+       
+        [HttpGet("{id:int}", Name = "Get")]
+
+        public ActionResult GetById(int id)
+        {
+            //add validation
+            List<MonitorRecord> records = Monitoring.Persistance.SqlCommandUtility.GetEntriesByAppId(id, _configuration);
+            records = records.OrderByDescending(x => x.ModifiedDate).ToList();
+            return Ok(records);
+
+        }
+
+        // POST: api/Monitoring
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult Post([FromBody] MonitorRecord monitorRecord)
+        {
+            //add validation
+            Monitoring.Persistance.SqlCommandUtility.AddMonitorRecordEntry(monitorRecord, _configuration);
+            return Ok();
+        }
+                
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id:int}",Name ="delete")]
+        public ActionResult  Delete(int id)
+        {
+            //add validation
+            Monitoring.Persistance.SqlCommandUtility.DeleteMonitorRecordsBasedOnAppId(id, _configuration);
+            return Ok();
+        }
+    }
+
+}
---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Monitoring.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Monitoring;
+
+/* https://docs.microsoft.com/en-us/dotnet/api/microsoft.data.sqlclient.sqlcommand?view=sqlclient-dotnet-core-1.1 */
+
+
+namespace Monitoring.Persistance
+{
+    public static class SqlCommandUtility
+    {
+
+        public static List<MonitorRecord> ExecuteDataReader(string query,IConfiguration config)
+        {
+            List<MonitorRecord> monitorRecords = new List<MonitorRecord>();
+            
+            using(SqlCommand command = new SqlCommand(query, GetSqlConnection(config)))
+            {
+                command.Connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    MonitorRecord monitorRecord = new MonitorRecord();
+                    monitorRecord.ID = reader.GetInt32(0);
+                    monitorRecord.AppID = reader.GetInt32(1);
+                    monitorRecord.AppArea = reader.IsDBNull(2) ? string.Empty : reader.GetString(2);
+                    monitorRecord.Information = reader.IsDBNull(3) ? string.Empty : reader.GetString(3);
+                    monitorRecord.Comments = reader.IsDBNull(4) ? string.Empty : reader.GetString(4);
+                    monitorRecord.Error = reader.GetBoolean(5);
+                    monitorRecord.ModifiedDate = reader.GetDateTime(6);
+
+                    monitorRecords.Add(monitorRecord);
+                }
+            }
+
+            return monitorRecords;
+
+        }
+
+        public static bool ApplicationIdExists(int id,IConfiguration config)
+        {
+            string query = $"select count(*) from {Constants.Persistance._applicationTableName} where appId={id}";
+
+            object obj = ExecuteScalar(query, config);
+
+            return Convert.ToInt32(obj) > 0;
+        }
+
+        public static List<MonitorRecord> GetEntriesByAppId(int Id,IConfiguration config)
+        {
+            List<MonitorRecord> monitorRecords = new List<MonitorRecord>();
+            string query = $"select * from {Constants.Persistance._monitoringTableName} where appId={Id}";
+
+            return ExecuteDataReader(query,config);
+        }
+
+        public static List<MonitorRecord> GetAll(IConfiguration config)
+        {
+            string commandText = $"select * from {Constants.Persistance._monitoringTableName}";
+            return ExecuteDataReader(commandText,config);
+
+        }
+
+        public static void AddMonitorRecordEntry(MonitorRecord monitorRecord,IConfiguration config)
+        {
+            string commandText = $"INSERT INTO {Constants.Persistance._monitoringTableName} VALUES (@AppId, @AppArea, @Information,@Comments,@Error,getdate())";
+
+            using(SqlCommand command = new SqlCommand(commandText, GetSqlConnection(config)))
+            {
+                command.Parameters.AddWithValue("@AppId",monitorRecord.AppID);
+                command.Parameters.AddWithValue("@AppArea", PrepareInputString(monitorRecord.AppArea));
+                command.Parameters.AddWithValue("@Information", PrepareInputString(monitorRecord.Information));
+                command.Parameters.AddWithValue("@Comments", PrepareInputString(monitorRecord.Comments));
+                command.Parameters.AddWithValue("@Error",monitorRecord.Error);
+                command.Connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+        public static int DeleteMonitorRecordsBasedOnAppId(int Id,IConfiguration config)
+        {
+            string commandText = $"delete from {Constants.Persistance._monitoringTableName} where id={Id}";
+            return ExecuteNonQuery(commandText,config);
+        }
+
+        public static int ExecuteNonQuery(string commandText,IConfiguration config)
+        {
+            using(SqlConnection conn = GetSqlConnection(config))
+            {
+                using(SqlCommand com = new SqlCommand(commandText, conn))
+                {
+                    com.Connection.Open();
+                    return com.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string commandText,IConfiguration config)
+        {
+            using(SqlConnection conn = GetSqlConnection(config))
+            {
+                using(SqlCommand com = new SqlCommand(commandText, conn))
+                {
+                    com.Connection.Open();
+                    return com.ExecuteScalar();
+                }
+            }
+        }
+
+        public static SqlConnection GetSqlConnection(IConfiguration config)
+        {
+            string connectionString=config.GetConnectionString("DefaultConnection");            
+            return new SqlConnection(connectionString);
+        }
+
+        public static string PrepareInputString(string input)
+        {
+            
+            return string.IsNullOrWhiteSpace(input) ? Constants.Persistance._noStringEntry : input;
+        }
+
+    }
+}
